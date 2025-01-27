65785840da5164388c4f67d248dac814eb972e78
Eric Flumerfelt <eflumerf@fnal.gov>
2025-01-27 14:43:11 -0600

    Use ReusableThread from the utilities package

--- plugins/DataRecorderModule.hpp
+++ plugins/DataRecorderModule.hpp
@@ -15,7 +15,7 @@
 #include "datahandlinglibs/concepts/RecorderConcept.hpp"
 #include "datahandlinglibs/recorderconfig/Structs.hpp"
 #include "datahandlinglibs/utils/BufferedFileWriter.hpp"
-#include "datahandlinglibs/utils/ReusableThread.hpp"
+#include "utilities/ReusableThread.hpp"
 
 #include <atomic>
 #include <fstream>
